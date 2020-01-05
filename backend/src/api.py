c5e8bdf610a2be22c5df8d5ec4c53a8658ffed94
alessandrome <mesti.alessandro.git@gmail.com>
2020-01-05 21:10:06 +0100

    Refactor code and adding empty methods for routings

--- backend/src/api.py
+++ backend/src/api.py
@@ -18,7 +18,7 @@ CORS(app)
 '''
 # db_drop_and_create_all()
 
-## ROUTES
+# ROUTES
 '''
 @TODO implement endpoint
     GET /drinks
@@ -27,6 +27,9 @@ CORS(app)
     returns status code 200 and json {"success": True, "drinks": drinks} where drinks is the list of drinks
         or appropriate status code indicating reason for failure
 '''
+@app.route('/drinks')
+def get_drinks():
+    pass
 
 
 '''
@@ -37,6 +40,9 @@ CORS(app)
     returns status code 200 and json {"success": True, "drinks": drinks} where drinks is the list of drinks
         or appropriate status code indicating reason for failure
 '''
+@app.route('/drinks-detail')
+def get_drink_details():
+    pass
 
 
 '''
@@ -48,6 +54,9 @@ CORS(app)
     returns status code 200 and json {"success": True, "drinks": drink} where drink an array containing only the newly created drink
         or appropriate status code indicating reason for failure
 '''
+@app.route('/drinks', methods=['POST'])
+def create_drink():
+    pass
 
 
 '''
@@ -61,6 +70,9 @@ CORS(app)
     returns status code 200 and json {"success": True, "drinks": drink} where drink an array containing only the updated drink
         or appropriate status code indicating reason for failure
 '''
+@app.route('/drinks/<int:drink_id>', methods=['PATCH'])
+def edit_drink(drink_id):
+    pass
 
 
 '''
@@ -73,6 +85,9 @@ CORS(app)
     returns status code 200 and json {"success": True, "delete": id} where id is the id of the deleted record
         or appropriate status code indicating reason for failure
 '''
+@app.route('/drinks/<int:drink_id>', methods=['DELETE'])
+def delete_drink(drink_id):
+    pass
 
 
 ## Error Handling
--- backend/src/database/models.py
+++ backend/src/database/models.py
@@ -9,31 +9,34 @@ database_path = "sqlite:///{}".format(os.path.join(project_dir, database_filenam
 
 db = SQLAlchemy()
 
-'''
-setup_db(app)
-    binds a flask application and a SQLAlchemy service
-'''
+
 def setup_db(app):
+    """
+    setup_db(app)
+        binds a flask application and a SQLAlchemy service
+    """
     app.config["SQLALCHEMY_DATABASE_URI"] = database_path
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     db.app = app
     db.init_app(app)
 
-'''
-db_drop_and_create_all()
-    drops the database tables and starts fresh
-    can be used to initialize a clean database
-    !!NOTE you can change the database_filename variable to have multiple verisons of a database
-'''
+
 def db_drop_and_create_all():
+    """
+    db_drop_and_create_all()
+        drops the database tables and starts fresh
+        can be used to initialize a clean database
+        !!NOTE you can change the database_filename variable to have multiple verisons of a database
+    """
     db.drop_all()
     db.create_all()
 
-'''
-Drink
-a persistent drink entity, extends the base SQLAlchemy Model
-'''
+
 class Drink(db.Model):
+    """
+    Drink
+    a persistent drink entity, extends the base SQLAlchemy Model
+    """
     # Autoincrementing, unique primary key
     id = Column(Integer().with_variant(Integer, "sqlite"), primary_key=True)
     # String Title
